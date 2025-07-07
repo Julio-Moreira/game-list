6999405236b3f83b6bf926c7dee50892974fac99
Julio <moreira.julio@proton.me>
2025-07-07 12:22:41 -0300

    Update rodape.php

--- modelo/rodape.php
+++ modelo/rodape.php
@@ -5,9 +5,9 @@
     echo "<footer style='text-align: center; font-size: 0.6em;'>";
 
     echo "<p>Desenvolvido por <strong>Julio</strong> &copy; ".date('Y')."</p>";
-    echo "<p>Baseado no <a href='https://www.estudonauta.com' target='_blank'>estudonauta</a></p>";
+    echo "<p>Baseado no curso PHP com Mysql do <a href='https://www.estudonauta.com' target='_blank'>estudonauta</a></p>";
     
     echo "</footer>";
-    $banco -> close();
+    $banco->close();
     
 ?>
